15c30d2e2c5a21c56a81fb4264b284ed25d39f43
Tigris <tabula.tenebris@gmail.com>
2021-11-14 04:09:03 +0600

    Add project files.

---
+++ InterfaceHierarchy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitmapImage myBitmap = new BitmapImage();
+            myBitmap.Draw();
+            myBitmap.DrawInBoundingBox(10, 10, 100, 150);
+            myBitmap.DrawUpsideDown();
+
+            IAdvancedDraw iAdvDraw = myBitmap as IAdvancedDraw;
+            if (iAdvDraw != null)
+                iAdvDraw.DrawUpsideDown();
+            IDrawable iAdvDraw1 = myBitmap as IDrawable;
+            if (iAdvDraw != null)
+                iAdvDraw.Draw();
+            Console.WriteLine();
+        }
+    }
+}
